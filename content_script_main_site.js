44fcea4fc2999d6b2cf1c107a6f0b2a8e7b252a3
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2020-04-28 20:21:28 +0530

    Add content script for main page

---
+++ content_script_main_site.js
@@ -0,0 +1,28 @@
+async function updateNumbeStatus(number){
+        // Update status in table for numbers which have no status 
+        var elems = document.querySelectorAll("td[data-type='status']")
+        for (var i=0; i< elems.length; i++){
+            elem = elems[i];
+            if (!(elem.innerHTML.toString().trim())){
+                // No status added right now add one
+                numberInElem = elem.getAttribute("data").toString().trim()
+                if (numberInElem === number){
+                    elem.innerHTML = "<img src='https://raw.githubusercontent.com/DefCon-007/WhatSend/master/images/Check.svg?sanitize=true' />"
+                }else {
+                    elem.innerHTML = "<img src='https://raw.githubusercontent.com/DefCon-007/WhatSend/master/images/Warning.svg?sanitize=true' />"
+                }
+            }
+        }
+
+}
+
+// Listen for messages from background script to update number status
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log("Message received");
+        if (request.type === "MESSAGE_SENT"){
+            // Message was successfully sent 
+            console.log("Updating number status")
+            updateNumbeStatus(request.number);
+        }
+    });
---
+++ content_script_whatsapp_web.js
@@ -0,0 +1,78 @@
+var numberPattern = /\d+/g;
+var storedNum = "";
+var functionRunning = false;
+
+function getElementByXpath(xpath){
+    return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+async function waitForElementToHide(selector, mainDivElem) {
+    while (true) {
+        var messageWaitingElement = document.querySelector(selector)
+        if (messageWaitingElement == null) {
+            // The waiting clock icon disappeared, message is sent
+            await new Promise(r => setTimeout(r, 2000));
+            var number = "UNKNOWN_NUMBER";
+            if (mainDivElem){
+                var numElem = mainDivElem.querySelector("span[dir='auto']");
+                var numElemText = numElem.innerText
+                var number = numElemText.match(numberPattern).join('');
+            }
+            updateNumberState(number);
+            return;
+        }
+        else {
+            // The waiting clock icon is still visible, wait for it to hide
+            await new Promise(r => setTimeout(r, 1000));
+        }
+    }
+}
+
+async function clickAndClose(element) {
+    await new Promise(r => setTimeout(r, 1000));
+    var mainDivElem = element.closest("div#main")
+    // Click the send button
+    element.click();
+    //Wait for clock icon(which indicaties the message is processing) to hide 
+    waitForElementToHide("span[data-icon='msg-time']", mainDivElem)
+    return;
+}
+
+function updateNumberState(number) {
+    // Send message to background script indicating message is sent
+    chrome.runtime.sendMessage({type: "MESSAGE_SENT", number:number}, function(response) {
+        window.close();
+      });
+}
+
+async function waitForElementToDisplay(selector, func) {
+    var xpath_invalid_phone_num = "//div[contains(text(),'Phone number shared via url is invalid')]"  
+    while (true) {
+        var element = document.querySelector(selector)
+        var phoneElem = getElementByXpath(xpath_invalid_phone_num)
+        if (element != null) {
+            func(element);
+            return;
+        }
+        else if (phoneElem){
+            //The phone number shared is invalid, close the current tab and continue
+            window.close();
+            return;
+        }
+        else {
+            await new Promise(r => setTimeout(r, 1000));
+        }
+    }
+}
+
+chrome.storage.local.get(["shouldAutoSend"], function (result) {
+    console.log("Getting value from local storage");
+    if (result.shouldAutoSend){
+        // Auto send is enabled, call the wait function!
+        console.log("Autosending the message!");
+        waitForElementToDisplay("span[data-icon='send']", clickAndClose)
+    } else [
+        console.log("Autosending is disabled")
+    ]
+});
+
