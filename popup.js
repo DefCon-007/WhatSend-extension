a0b7bc7b9d9777c1085a97ea8684c902c055a2a5
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2020-04-28 20:00:27 +0530

    Update popup to display buttons and store configuration in local storage

--- popup.js
+++ popup.js
@@ -11,81 +11,24 @@ try {
     }
 }
 
+
 function setCheckedState(state) {
     console.log("setting teh state");
     console.log(state)
-    var elem = document.getElementById('myonoffswitch');
-    elem.checked = state['shouldAutoSend'];
+    var elem = document.getElementById('onoffswitch');
+    elem.checked = state.shouldAutoSend;
 }
+browserApi.storage.local.get(['shouldAutoSend'], setCheckedState)
 
-
-chrome.storage.local.get(['shouldAutoSend'], setCheckedState)
-
-
-var elem = document.getElementById('myonoffswitch');
-
-
+// Setup the checkbox
+var elem = document.getElementById('onoffswitch');
 elem.addEventListener('change', (event) => {
+    // Checkbox value changed!!!
     console.log("settigns")
-    chrome.storage.local.set({ shouldAutoSend: event.target.checked })
-    if (event.target.checked) {
-        alert('checked');
-    } else {
-        alert('not checked');
-    }
+    browserApi.storage.local.set({ shouldAutoSend: event.target.checked })
 })
 
-
-// console.log *
-// browserApi.tabs.create({
-//     url: "https://web.whatsapp.com/send?phone=918768668885&text=Hi This is fucking test",
-//     active: true
-//   });
-
-// chrome.tabs.getCurrent(function (tab) {
-//     //Your code below...
-//     var tabUrl = encodeURIComponent(tab.url);
-//     var tabTitle = encodeURIComponent(tab.title);
-//     var myNewUrl = "https://web.whatsapp.com/send?phone=918768668885&text=Hi This is fucking test"
-
-//     //Update the url here.
-//     chrome.tabs.update(tab.id, {url: myNewUrl});
-//   });
-
-// var msgList = [
-//     "odjnv",
-//     "ojfsnv",
-//     "pojisnfvof",
-//     "ojwfbnv",
-//     "owjf"
-// ]
-
-
-// for (var i = 0; i < msgList.length; i++) {
-// chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
-//     // chrome.tabs.update(tab.id, {url: `https://web.whatsapp.com/send?phone=918768668885&text=$msgList[i]`});
-//     chrome.tabs.update(tab.id, {url: `https://web.whatsapp.com/send?phone=919098854207&text=$msgList[i]`});
-// });
-
-
-// adding listener to your button in popup window
-// document.getElementById('press').addEventListener('click', injectTheScript);
-console.log("nosio nsiniwnr noiiwn ownr orwun oern oeunr uoern ro")
-function injectTheScript() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        // query the active tab, which will be only one tab
-        //and inject the script in it
-        for (var i = 0; i < tabs.length; i++) {
-            var tab = tabs[i];
-            if ("web.whatsapp.com" in tab.url) {
-                chrome.tabs.executeScript(tabs[0].id, { file: "content_script.js" });
-            }
-        }
-
-    });
-}
-injectTheScript();
-
-
-//   }
-
+var button = document.getElementById("websiteButton");
+button.addEventListener("click", function(){
+    browserApi.tabs.create({url:"https://whatsend.defcon007.com/"});
+});
