86e49c2f4ee8c50baa5affb09724e53c6d7c5fb8
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2020-04-28 23:45:58 +0530

    Add logging and update manifest

--- background.js
+++ background.js
@@ -2,15 +2,22 @@ chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.type === "MESSAGE_SENT") {
             // A message was successfully sent
+            console.log("Received the message")
+            console.log(request)
             if (request.number) {
                 chrome.tabs.query({ currentWindow: true }, function (tabs) {
                     for (var i = 0; i < tabs.length; i++) {
                         var tab = tabs[i];
-                        console.log(tab.url)
-                        if ((tab.url.toLowerCase().includes("whatsend.defcon007.com")) || (tab.url.toLowerCase().includes("whats-sender-website"))) {
-                            console.log(`Sending the message with number ${request.number}`)
-                            chrome.tabs.sendMessage(tab.id, { greeting: "Message from backgrond", type: "MESSAGE_SENT", number: request.number }, function (response) {
-                            });
+                        try {
+                            if ((tab.url.toLowerCase().includes("whatsend.defcon007.com")) || (tab.url.toLowerCase().includes("whats-sender-website"))) {
+                                console.log(`Sending the message with number ${request.number}`)
+                                chrome.tabs.sendMessage(tab.id, { type: "UPDATE_STATE", number: request.number }, function (response) {
+                                });
+                            }
+                        }
+                        catch (err) {
+                            console.log("error occured")
+                            console.log(err.message)
                         }
                     }
                 });
--- content_script_main_site.js
+++ content_script_main_site.js
@@ -1,3 +1,5 @@
+console.log("Script injected")
+
 async function updateNumbeStatus(number) {
     // Update status in table for numbers which have no status 
     var elems = document.querySelectorAll("td[data-type='status']")
@@ -8,6 +10,7 @@ async function updateNumbeStatus(number) {
             numberInElem = elem.getAttribute("data").toString().trim()
             if (numberInElem === number) {
                 elem.innerHTML = "<img src='https://raw.githubusercontent.com/DefCon-007/WhatSend/master/images/Check.svg?sanitize=true' />"
+                elem.setAttribute("data-sort", "success");
             } else {
                 elem.innerHTML = "<img src='https://raw.githubusercontent.com/DefCon-007/WhatSend/master/images/Warning.svg?sanitize=true' />"
             }
@@ -20,7 +23,7 @@ async function updateNumbeStatus(number) {
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         console.log("Message received");
-        if (request.type === "MESSAGE_SENT") {
+        if (request.type === "UPDATE_STATE") {
             // Message was successfully sent 
             console.log("Updating number status")
             updateNumbeStatus(request.number);
